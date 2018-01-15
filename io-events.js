daa4b712a4a50688086ddd3ce486bd5eeac38232
David Vezzani <david_vezzani@yahoo.com>
2018-01-15 08:57:40 -0700

    example event listener and emitter

--- io-events.js
+++ io-events.js
@@ -7,6 +7,12 @@ module.exports = function(server) {
   
   io.on('connection', function(socket){
     console.log('a user connected');
+
+    socket.on('emit_method', function(msg){
+      console.log("received 'emit_method' with: " + JSON.stringify(msg));
+      io.emit('got_it', msg);
+    });
+      
   });
 };
 
