6ac57160087928faecf2d7b2dfab6f48d9022fa5
David Vezzani <david_vezzani@yahoo.com>
2018-01-15 13:24:12 -0700

    CRUD for user

--- io-events.js
+++ io-events.js
@@ -12,6 +12,27 @@ module.exports = function(server) {
       console.log("received 'emit_method' with: " + JSON.stringify(msg));
       io.emit('got_it', msg);
     });
+
+    socket.on('create_user', function(user){
+      new models.User().save(user).then(() => {
+        io.emit('user_created', {action: 'create', type: 'user', model: user});
+      })
+    });
+      
+    socket.on('update_user', function(user){
+      new models.User({id: user.id}).save(user).then(() => {
+        io.emit('user_updated', {action: 'update', type: 'user', model: user});
+      })
+    });
+      
+    socket.on('destroy_user', function(user){
+      var _model = null;
+      new models.User({id: user.id}).fetch()
+      .then((model) => { _model = model; return new models.User({id: user.id}).destroy() })
+      .then(() => {
+        io.emit('user_destroyed', {action: 'destroy', type: 'user', model: _model});
+      })
+    });
       
   });
 };
